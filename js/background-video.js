4b8c6fe0160bb41381ce02ad2aecc13bf5b78d64
oleg <zavalkin06@gmail.com>
2019-02-28 19:12:53 +0200

    структуризация гита

--- js/background-video.js
+++ js/background-video.js
@@ -1,9 +1,9 @@
-$(document).ready(function() {
+$(document).ready(function () {
   var videobackground = new $.backgroundVideo('$(body)', {
     "width": 1280,
     "height": 720,
     "path": "media/",
-    "filename": "cloud",
-    "types": ["mp4","ogg","webm"]
+    "filename": "hd0933",
+    "types": ["mp4", "ogg", "webm"]
   });
 });
