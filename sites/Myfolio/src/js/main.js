867790aadf03956ebea2bb313a31dfbbcc3bad50
oleg <zavalkin06@gmail.com>
2019-03-22 15:10:00 +0200

    готов адаптив

--- sites/Myfolio/src/js/main.js
+++ sites/Myfolio/src/js/main.js
@@ -7,3 +7,11 @@ function HeadHeight() {
     var wHeight = $(window).height();
     $('#main').css('height', wHeight);
 }
+
+var trigger = document.querySelector('.trigger')
+
+trigger.addEventListener('click', function () {
+    trigger.classList.toggle("trigger--show");
+    $(".departs").toggleClass('active');
+
+});
